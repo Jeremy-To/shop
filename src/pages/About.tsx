4bfc6780c216d83ea7642e7ee786b37c6a46b7b0
jeremy-to <jeremy_to@outlook.fr>
2023-01-27 14:41:19 +0100

    update of the store

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -3,40 +3,41 @@ import { Col, Row } from 'react-bootstrap';
 export function About() {
 	return (
 		<>
-			<h1>About us</h1>
+			<h1 className="d-flex justify-content-center align-items-center">
+				About us
+			</h1>
+			<br />
 			<Row md={2} xs={1} lg={3} className="g-3">
 				<Col>
-					<h2>Ici, vous êtes en lien avec des libraires</h2>
+					<h2>Here, you are in contact with booksellers</h2>
 					<p>
-						Le principe de notre site de vente de livres repose sur
-						l'utilisation des canaux modernes d'informations et de commandes à
-						travers un site doté d'une base de données bibliographiques d'un
-						million de références, d'une plate-forme logistique avec un stock de
-						près de 350 000 références, de l'expertise de ses nombreux libraires
-						(pas des algorithmes) et enfin plusieurs milliers de livres
-						chroniqués pour mieux vous conseiller.
+						The principle of our site for the sale of books is based on the use
+						of modern channels of information and orders through a site equipped
+						with a bibliographic database of one million references, a logistics
+						platform with a stock of nearly 350,000 references, the expertise of
+						its many booksellers (not algorithms) and finally several thousand
+						books reviewed to better advise you.
 					</p>
 				</Col>
 				<Col>
-					<h2>Ici, vous encouragez des acteurs locaux</h2>
+					<h2>Here, you encourage local actors</h2>
 					<p>
-						Domiciliés en France, nous sommes intégralement soumis à la
-						fiscalité française. Notre PME qui emploie uniquement des personnes
-						avec des contrats à durée indéterminée permet à des milliers de
-						libraires de continuer à vous proposer des livres dans plus de 750
-						communes en France et en Belgique.
+						Domiciled in France, we are fully subject to French taxation. Our
+						SME, which only employs people with permanent contracts, allows
+						thousands of booksellers to continue to offer you books in more than
+						750 municipalities in France and Belgium.
 					</p>
 				</Col>
 				<Col>
-					<h2>Ici, vous renforcez le lien social</h2>
+					<h2>Here, you strengthen the social bond</h2>
 					<p>
-						En France et en Belgique*, les livres sont mis à disposition chez
-						l'un des 2500 Points Libraires partenaires qui ont choisi de
-						participer à cette démarche collective et inédite de la librairie
-						indépendante. En retirant vos livres commandés sur lalibrairie.com,
-						vous allez à la rencontre d'un authentique libraire, vous favorisez
-						le commerce de proximité, celui qui contribue à l'aménagement
-						culturel du territoire et qui anime les quartiers.
+						In France and Belgium, the books are made available at one of the
+						2,500 partner Bookseller Points who have chosen to participate in
+						this collective and unprecedented approach to independent
+						bookstores. By picking up your books ordered on lalibrairie.com, you
+						are going to meet an authentic bookseller, you are promoting local
+						trade, the one that contributes to the cultural development of the
+						territory and which animates the districts.
 					</p>
 				</Col>
 			</Row>
