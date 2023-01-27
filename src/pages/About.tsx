bfd7e8f4f83526afad70a0fa72e47ea0b1b8b1a7
jeremy-to <jeremy_to@outlook.fr>
2023-01-27 14:24:01 +0100

    update Home and About pages

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,3 +1,45 @@
+import { Col, Row } from 'react-bootstrap';
+
 export function About() {
-  return <h1>About</h1>
+	return (
+		<>
+			<h1>About us</h1>
+			<Row md={2} xs={1} lg={3} className="g-3">
+				<Col>
+					<h2>Ici, vous êtes en lien avec des libraires</h2>
+					<p>
+						Le principe de notre site de vente de livres repose sur
+						l'utilisation des canaux modernes d'informations et de commandes à
+						travers un site doté d'une base de données bibliographiques d'un
+						million de références, d'une plate-forme logistique avec un stock de
+						près de 350 000 références, de l'expertise de ses nombreux libraires
+						(pas des algorithmes) et enfin plusieurs milliers de livres
+						chroniqués pour mieux vous conseiller.
+					</p>
+				</Col>
+				<Col>
+					<h2>Ici, vous encouragez des acteurs locaux</h2>
+					<p>
+						Domiciliés en France, nous sommes intégralement soumis à la
+						fiscalité française. Notre PME qui emploie uniquement des personnes
+						avec des contrats à durée indéterminée permet à des milliers de
+						libraires de continuer à vous proposer des livres dans plus de 750
+						communes en France et en Belgique.
+					</p>
+				</Col>
+				<Col>
+					<h2>Ici, vous renforcez le lien social</h2>
+					<p>
+						En France et en Belgique*, les livres sont mis à disposition chez
+						l'un des 2500 Points Libraires partenaires qui ont choisi de
+						participer à cette démarche collective et inédite de la librairie
+						indépendante. En retirant vos livres commandés sur lalibrairie.com,
+						vous allez à la rencontre d'un authentique libraire, vous favorisez
+						le commerce de proximité, celui qui contribue à l'aménagement
+						culturel du territoire et qui anime les quartiers.
+					</p>
+				</Col>
+			</Row>
+		</>
+	);
 }
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,3 +1,19 @@
+import { Link } from "react-router-dom"
+
 export function Home() {
-  return <h1>Home</h1>
+	return (
+		<div>
+			<h1 className="d-flex justify-content-center align-items-center">
+				Welcome to my shop !
+			</h1>
+			<div className="d-flex flex-wrap position-relative justify-content-center align-items-center">
+				<div className="position-absolute p-3 bg-light text-black rounded"><Link to="/Store">Go to store</Link></div>
+				<img
+					src="imgs/LibraryShop.jpg"
+					alt="Image of a library shop"
+					className="flex-fill"
+				/>
+			</div>
+		</div>
+	);
 }
